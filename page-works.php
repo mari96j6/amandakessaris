1541fe06a3a6cdff905d104303e094119a23a7e1
Marius <mari96j6@stud.kea.dk>
2022-12-15 21:33:43 +0100

    klar til spørgsmål til lærerne

--- page-works.php
+++ page-works.php
@@ -14,33 +14,62 @@ get_header();
 
 <section id="primary" class="content-area">
 <main id="main" class="site-main">
-
+<nav id="filtrering">
+    <button data-work="alle">Alle</button>
+</nav>
 <section class="workcontainer"></section>
 </main>
 <script>
 let works;
+let categories;
+let filterWork = "alle";
 
 const dbUrl="https://mariusdesign.dk/kea/10_eksamensprojekt/amandakessaris/wp-json/wp/v2/work?per_page=100";
+const catUrl="https://mariusdesign.dk/kea/10_eksamensprojekt/amandakessaris/wp-json/wp/v2/categories";
 
 async function getJson(){
         const data = await fetch(dbUrl);
+        const catdata = await fetch(catUrl);
         works = await data.json();
+        categories = await catdata.json();
 console.log(works);
 visWorks();
+opretKnapper();
     }
+function opretKnapper(){
+    categories.forEach(cat=>{
+    document.querySelector(#"filtrering").innerHTML += <button class="filter" data-work="${cat.id}">${cat.name}</button>
+    })
 
-    function visWorks(){
+    addEventListenerToButtons();
+
+}
+
+function addEventListenerToButtons(){
+    document.querySelectorAll("#filtrering button").forEach(elm =>{
+        elm.addEventListener("click, filtrering");
+    })
+};
+
+function filtrering(){
+filterWork = this.dataset.work;
+console.log(filterWork);
+
+visWorks();
+}
+    
+function visWorks(){
         let temp = document.querySelector("template");
-let container = document.querySelector(".workscontainer")
+let container = document.querySelector("#workscontainer")
+container.innerHTML = "";
 works.forEach(work=>{
-let klon = temp.cloneNode(true).content;
+    if (filterWork=="alle" || work.categories.includes(parseInt(filterWork))){
+            let klon = temp.cloneNode(true).content;
             klon.querySelector("h2").textcontent = work.title.rendered;
-klon.querySelector("img").src = work.billede.guid;
-klon.querySelector("article").addEventListener("click", ()=>{location.href="restdb-single.html?id="+work._id})
-        
-
+            klon.querySelector("img").src = work.billede.guid;
+            klon.querySelector("article").addEventListener("click", ()=>{location.href="restdb-single.html?id="+work._id})
             container.appendChild(klon);
-
+        }
         })
     }
 
